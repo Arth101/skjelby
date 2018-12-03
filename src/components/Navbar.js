112e1f13a269681a6caf0e1ae7f25ac92aed9184
Anders Enghøj <andersenghoej@gmail.com>
2018-12-03 14:41:42 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,17 +12,11 @@ const Navbar = () => (
         </Link>
       </div>
       <div className="navbar-start">
+        <Link className="navbar-item" to="/tags/resume/">
+          Sessions
+        </Link>
         <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-        <Link className="navbar-item" to="/contact">
-          Contact
-        </Link>
-        <Link className="navbar-item" to="/contact/examples">
-          Form Examples
+          Om Kampagnen
         </Link>
       </div>
       <div className="navbar-end">
