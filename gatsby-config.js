781db9f1532abd1badcd5d2b08557eb6819caf9e
Anders Enghøj <andersenghoej@gmail.com>
2019-04-02 12:33:55 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,13 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
-    'gatsby-plugin-purgecss', // must be after other CSS plugins
+    {
+      // must be after other CSS plugins
+      resolve: 'gatsby-plugin-purgecss',
+      options: {
+        whitelist: ['table']
+      }
+    },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
