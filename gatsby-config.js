f597457edbc5c187bd61ab867209313ad9040e53
Anders Enghøj <andersenghoej@gmail.com>
2019-04-02 12:55:44 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
       // must be after other CSS plugins
       resolve: 'gatsby-plugin-purgecss',
       options: {
-        whitelist: ['table']
+        whitelist: ['table', 'th', 'td']
       }
     },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
