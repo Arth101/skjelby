e30cec964c00e5d212e7d60e437f73ccb9697172
Anders Enghøj <andersenghoej@gmail.com>
2019-04-08 14:32:54 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,11 @@ import { Link } from 'gatsby'
 import dndBeyondLogo from '../img/dnd-beyond--small.png'
 import logo from '../img/logo.png'
 
-state = {
+var state = {
   isActive: false,
 }
 
-toggleNav = () => {
+var toggleNav = () => {
   this.setState(prevState => ({
     isActive: !prevState.isActive
   }))
@@ -20,13 +20,13 @@ const Navbar = () => (
         <Link to="/" className="navbar-item" title="Logo">
           <img src={logo} alt="skjelby" style={{ width: '88px' }} />
         </Link>
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <a role="button" class="navbar-burger burger" onClick={this.toggleNav} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
-      <div id="navbarBasicExample" class="navbar-menu">
+      <div id="navbarBasicExample" className={ this.state.isActive ? 'navbar-menu is-active' : 'navbar-menu'}>
         <div className="navbar-start">
           <Link className="navbar-item" to="/tags/resume/">
             Sessions
