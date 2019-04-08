7dcc22fe0bd0764bb187077a0be187fd97299187
Anders Enghøj <andersenghoej@gmail.com>
2019-04-08 14:05:33 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,26 +10,33 @@ const Navbar = () => (
         <Link to="/" className="navbar-item" title="Logo">
           <img src={logo} alt="skjelby" style={{ width: '88px' }} />
         </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/tags/resume/">
-          Sessions
-        </Link>
-        <Link className="navbar-item" to="/about">
-          Level progression
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
         </a>
+      </div>
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div className="navbar-start">
+          <Link className="navbar-item" to="/tags/resume/">
+            Sessions
+          </Link>
+          <Link className="navbar-item" to="/about">
+            Level progression
+          </Link>
+        </div>
+        <div className="navbar-end">
+          <a
+            className="navbar-item"
+            href="https://www.dndbeyond.com/campaigns/152997"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="icon">
+              <img src={github} alt="Github" />
+            </span>
+          </a>
+        </div>
       </div>
     </div>
   </nav>
