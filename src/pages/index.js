db4e0f06fa7788ff412ae87dab3e98b16f1d2956
Anders Enghøj <andersenghoej@gmail.com>
2019-04-12 09:47:35 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default class IndexPage extends React.Component {
     return (
       <Layout>
         <section class="hero is-primary is-bold">
-          <div className="hero-body">
+          <div className="hero-body hero-frontpage">
             <div className="container">
               <p className="title">
                 Dungeons &amp; Dragons
