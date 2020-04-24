8a8348d69c4de6b9f51e94a841edb6906cf3bc3b
Anders Enghøj <andersenghoej@gmail.com>
2020-04-24 14:22:13 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default class IndexPage extends React.Component {
                 Dungeons &amp; Dragons
               </p>
               <p className="subtitle">
-                Fra Skjelby til Gobulus Khanatet
+                Fra Skjelby til Quekias jungle
               </p>
             </div>
           </div>
